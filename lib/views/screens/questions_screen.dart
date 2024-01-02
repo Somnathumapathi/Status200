a76a6d3ed32258bb85f4fefa4e91f702bf261de1
Somnathumapathi <somnathumapathi7@gmail.com>
2024-01-02 17:49:08 +0530

    Did some changes

--- lib/views/screens/questions_screen.dart
+++ lib/views/screens/questions_screen.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:status200/views/widgets/addquestiondialog.dart';
 
 class QuestionScreen extends StatelessWidget {
   const QuestionScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Get.dialog(AddQuestionDialog()),
+        child: Icon(Icons.add_comment),
+      ),
+    );
   }
 }
